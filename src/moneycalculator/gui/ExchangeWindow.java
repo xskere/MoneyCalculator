3ff46ac00a556be940d6084a222ad195af7a61d8
alero <alero@DESKTOP-SQ5BNGD>
2021-11-01 14:44:40 +0000

    An app that exchanges the most used currencies to EUR. The exchange rates are updated as of 30/10/2021

--- src/moneycalculator/gui/ExchangeWindow.java
+++ src/moneycalculator/gui/ExchangeWindow.java
@@ -18,8 +18,8 @@ public class ExchangeWindow extends javax.swing.JFrame {
     /**
      * Creates new form GUI
      */
-    public ExchangeWindow(List<Currency> currencies) {
-        this.currencies = currencies;
+    public ExchangeWindow() {
+        this.currencies = loadCurrencies("currencies.txt");
         initComponents();
     }
 
@@ -270,7 +270,7 @@ public class ExchangeWindow extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new ExchangeWindow(loadCurrencies("currencies.txt")).setVisible(true);
+                new ExchangeWindow().setVisible(true);
                 
             }
         });
